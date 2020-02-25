aa483a535bcabb2dd91224b5bc7dfbdcea819c05
Lucian D <lucian.davidescu@yahoo.ro>
2020-02-25 22:05:51 +0200

    #294 - added color for status column

--- src/components/cashout/CashoutElement.tsx
+++ src/components/cashout/CashoutElement.tsx
@@ -13,12 +13,21 @@ interface CashoutElementState {
 class CashoutElement extends React.Component<CashoutElementProps, CashoutElementState> {
 
     render(): React.ReactNode {
+        let statusColumn;
+        if (this.props.cashout.status === "PAID") {
+            statusColumn = <td style={{backgroundColor: "#6eff24"}}>{this.props.cashout.status}</td>;
+        } else if (this.props.cashout.status === "REJECTED") {
+            statusColumn = <td style={{backgroundColor: "#ff4c4f"}}>{this.props.cashout.status}</td>;
+        } else {
+            statusColumn = <td style={{backgroundColor: "#fffc82"}}>{this.props.cashout.status}</td>;
+        }
+
         return (
             <React.Fragment>
                 <tr className="tr-shadow">
                     <td>{new Date(parseFloat(this.props.cashout.createdAt._seconds) * 1000).toLocaleDateString('ro-RO', dateOptions)}</td>
                     <td>{this.props.cashout.userId}</td>
-                    <td>{this.props.cashout.status}</td>
+                    {statusColumn}
                     <td>{this.props.cashout.amount}</td>
                     <td>{this.props.cashout.target}</td>
                 </tr>
