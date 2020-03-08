820fafed1dd9d1d82255dffcdb4c2edb115eb6da
Lucian D <lucian.davidescu@yahoo.ro>
2020-03-08 11:04:14 +0200

    added process env for config

--- src/config/FirebaseConfig.tsx
+++ src/config/FirebaseConfig.tsx
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: 'AIzaSyDSVAwViUzdUBWIF_0af2g9LNef93t3qWU',
-    authDomain: 'charitydiscount-test.firebaseapp.com',
-    databaseURL: 'https://charitydiscount-test.firebaseio.com',
-    projectId: 'charitydiscount-test',
-    storageBucket: 'charitydiscount-test.appspot.com',
-    messagingSenderId: '328132170246',
-    appId: '1:328132170246:web:43b1a0706ad1d5757da987',
-    measurementId: 'G-G5WSP6RF8M'
+    apiKey: process.env.REACT_APP_FB_API_KEY,
+    authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FB_DB_URL,
+    projectId: process.env.REACT_APP_FB_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FB_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_FB_APP_ID,
+    measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID
 };
 
 export default firebaseConfig;
